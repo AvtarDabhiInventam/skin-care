c10a0c84498c2ddf72263b2fc6094046c5cdb710
Neel <neel@inventam.com>
2023-09-22 10:13:17 +0530

    inner banner

--- src/components/inner-banner/index.js
+++ src/components/inner-banner/index.js
@@ -1,7 +1,9 @@
+"use client"
 import React from "react";
 import aboutbannerimg from "../../images/about-desk-banner.png";
 import Image from "next/image";
 import styles from "./innerbanner.module.scss";
+import { Button } from "react-bootstrap";
 
 const InnerBanner = () => {
   return (
@@ -10,6 +12,7 @@ const InnerBanner = () => {
         <Image src={aboutbannerimg} alt="Banner" width={1920} height={1000} />
         <div className={`${styles.banner_contain_div}`}>
             <h2 className={`${styles.banner_heading}`}>A new PHILOSOPHY on SKINCARE.</h2>
+            <Button className="common-btn mt-4 bg-white" variant="light">buy now</Button>
         </div>
       </div>
     </>
