1c8bf4c8c4886d9c9b2d10a02028eee2d1df70b9
dhavalinventam <dhaval@inventam.com>
2023-09-25 14:05:53 +0530

    call to action section design

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,14 @@ import RecommendationProduct from "@/components/home/recommendation-product";
 import CareProduct from "@/components/care-product/care-product";
 import Testimonial from "@/components/testimonial/testimonial";
 import AboutProduct from "@/components/home/about-product";
+import Avtar from "@/components/home/avtar-sec";
 export default function Home() {
   return (
     <main>
       <Slider />
       <SkinType/>
       {/* <RecommendationProduct/> */}
+      <Avtar/>
       <CareProduct/>
       <Testimonial/>
       <AboutProduct/>
